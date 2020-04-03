084207faadc39f5ad68fe42d321ab84273d7bae4
Matze <st154919@stud.uni-stuttgart.de>
2020-04-03 19:19:28 +0200

    Implemented emergency stop

--- src/main/java/Test/TestConnection.java
+++ src/main/java/Test/TestConnection.java
@@ -1,7 +1,11 @@
 package Test;
 
+import java.io.IOException;
 import java.util.Scanner;
 
+import connection.ConnectionConstants;
+import connection.Sender;
+import connection.SocketConnector;
 
 public class TestConnection {
 	static Scanner sc;
@@ -11,22 +15,38 @@ public class TestConnection {
 	}
 
 	public static void main(String[] args) {
-//		SocketConnector.Connect();
+		SocketConnector.Connect();
 		System.out.println("Leertaste für Notaus drücken:");
 		emergencyStop();
 //		System.out.println(SocketConnector.getConStateStr());
 	}
+
 	/**
 	 * Method, that listens to a whitespace and the puts out a emergency stop
 	 */
 	private static void emergencyStop() {
 		sc = new Scanner(System.in);
 		String scanRes = sc.nextLine();
-		if (! scanRes.contains(" ") && scanRes.length() == 1) {
+		// exit programm if kill is entered
+		if (scanRes.toLowerCase().contains("help") && scanRes.length() == 4) {
+			System.out.println("-----------Hilfe-----------");
+			System.out.println("Kommandos: Leertaste -> Nothalt; exit-> Programmende;");
+			System.out.println("----------------------");
+			emergencyStop();
+		} else if (scanRes.toLowerCase().contains("exit") && scanRes.length() == 4) {
+			try {
+				SocketConnector.closeConnection();
+			} catch (IOException e) {
+			}
+			System.exit(0);
+		}
+		if (!scanRes.contains(" ") && scanRes.length() == 1) {
+			System.out.println("Nur Leertaste akzeptiert!");
 			emergencyStop();
 		} else {
-			System.out.println("Leertaste!");
-			
+			Sender.addMessageQueue(ConnectionConstants.EMERGENCY_STOP_MESSAGE);
+			emergencyStop();
 		}
+
 	}
 }
