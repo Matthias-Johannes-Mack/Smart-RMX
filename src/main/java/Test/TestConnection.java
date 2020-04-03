c4e922c1579669de4da17909a7740a2a6f65048a
Matze <st154919@stud.uni-stuttgart.de>
2020-04-03 17:42:26 +0200

    Added a check Spacebar

--- src/main/java/Test/TestConnection.java
+++ src/main/java/Test/TestConnection.java
@@ -1,13 +1,23 @@
 package Test;
+import java.util.Scanner;
+
 import connection.*;
 
 public class TestConnection {
+	static Scanner sc;
 	public TestConnection() {
 		
 	}
 
 	public static void main(String[] args) {
-		SocketConnector.Connect();
-		System.out.println(SocketConnector.getConStateStr());
+//		SocketConnector.Connect();
+		System.out.println("Eingabe:");
+		sc =  new Scanner(System.in);
+		String scanRes = sc.nextLine();
+		if (scanRes.contains(" ") && scanRes.length() == 1) {
+			System.out.println("Leertaste!");
+		}
+		System.out.println(scanRes);
+//		System.out.println(SocketConnector.getConStateStr());
 	}
 }
