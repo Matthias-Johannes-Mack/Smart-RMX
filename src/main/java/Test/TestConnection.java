c4f47b1db91167b32fe5b0afc36a199a13090699
Matze <st154919@stud.uni-stuttgart.de>
2020-04-03 17:54:44 +0200

    Emergency Stop branch

--- src/main/java/Test/TestConnection.java
+++ src/main/java/Test/TestConnection.java
@@ -1,22 +1,30 @@
 package Test;
+
 import java.util.Scanner;
 
-import connection.*;
 
 public class TestConnection {
 	static Scanner sc;
+
 	public TestConnection() {
-		
+
 	}
 
 	public static void main(String[] args) {
 //		SocketConnector.Connect();
-		System.out.println("Eingabe:");
-		sc =  new Scanner(System.in);
-		String scanRes = sc.nextLine();
-		if (scanRes.contains(" ") && scanRes.length() == 1) {
-			System.out.println("Leertaste!");
-		}
+		System.out.println("Leertaste für Notaus drücken:");
+		emergencyStop();
 //		System.out.println(SocketConnector.getConStateStr());
+	}
+
+	private static void emergencyStop() {
+		sc = new Scanner(System.in);
+		String scanRes = sc.nextLine();
+		if (! scanRes.contains(" ") && scanRes.length() == 1) {
+			emergencyStop();
+		} else {
+			System.out.println("Leertaste!");
+			
+		}
 	}
 }
