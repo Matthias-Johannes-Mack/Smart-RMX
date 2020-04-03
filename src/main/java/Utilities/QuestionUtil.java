6a33e6e065a2141f4b61ea79946ec4463918b36b
Matze <st154919@stud.uni-stuttgart.de>
2020-04-03 19:44:44 +0200

    Implemented emergency stop

--- src/main/java/Utilities/QuestionUtil.java
+++ src/main/java/Utilities/QuestionUtil.java
@@ -39,30 +39,30 @@ public class QuestionUtil {
 		}
 	}
 
-	/**
-	 * Method that retries the connection after a reload
-	 */
-	public static void retry_reload() {
-		// retry the connection, if possible
-		System.out.println("Erneut verbinden y/n?");
-		Scanner in = new Scanner(System.in);
-		String retryStr = in.nextLine().toLowerCase();
-		// reset the idle time
-		SocketConnector.setLastServerResponse(System.currentTimeMillis());
-		if (retryStr != null) {
-			switch (retryStr) {
-			case "y":
-				SocketConnector.Reload();
-				break;
-			case "n":
-				System.exit(0);
-				break;
-			// if the string is false retry
-			default:
-				System.out.println("Falschen Wert eingegeben!");
-				SocketConnector.Reload();
-				break;
-			}
-		}
-	}
+//	/**
+//	 * Method that retries the connection after a reload
+//	 */
+//	public static void retry_reload() {
+//		// retry the connection, if possible
+//		System.out.println("Erneut verbinden y/n?");
+//		Scanner in = new Scanner(System.in);
+//		String retryStr = in.nextLine().toLowerCase();
+//		// reset the idle time
+//		SocketConnector.setLastServerResponse(System.currentTimeMillis());
+//		if (retryStr != null) {
+//			switch (retryStr) {
+//			case "y":
+//				SocketConnector.Reload();
+//				break;
+//			case "n":
+//				System.exit(0);
+//				break;
+//			// if the string is false retry
+//			default:
+//				System.out.println("Falschen Wert eingegeben!");
+//				SocketConnector.Reload();
+//				break;
+//			}
+//		}
+//	}
 }
