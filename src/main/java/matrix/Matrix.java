b5d5f8328109154d410347baac630ea7cc0426b1
Matze <st154919@stud.uni-stuttgart.de>
2020-04-05 16:13:06 +0200

    Matrix test

--- src/main/java/matrix/Matrix.java
+++ src/main/java/matrix/Matrix.java
@@ -1,5 +1,52 @@
 package matrix;
 
-public class Matrix {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+/**
+ * Class for the Matrix
+ *
+ * @author Team SmartRMX
+ */
+public class Matrix {
+	// Arraysize of (112*8Bit)²
+	final int arraySize = 802816;
+	public Object[] matrix;
+	// Singleton-Pattern START -----------------------------------------
+
+	/**
+	 * Singleton instance of Matrix
+	 */
+	private static Matrix instance = null;
+
+	/**
+	 * private constructor to prevent instantiation
+	 */
+	private Matrix() {
+
+	}
+
+	/**
+	 * Returns singleton Matrix instance
+	 *
+	 * @return BusDepot Singleton instance
+	 */
+	public static synchronized Matrix getMatrix() {
+		if (instance == null) {
+			instance = new Matrix();
+			instance.createMatrix();
+		}
+		return instance;
+	}
+
+	// Singleton-Pattern END ________________________________________________
+	/**
+	 * Method, that creates the matrix
+	 */
+	private void createMatrix() {
+		// create the matrix with fixed arraySize
+		matrix = new Object[arraySize];
+		matrix[0] = "test";
+		matrix[8] = "9";
+	}
 }
---
+++ src/main/java/matrix/MatrixTest.java
@@ -0,0 +1,14 @@
+package matrix;
+
+import java.util.Arrays;
+
+public class MatrixTest {
+
+	public static void main(String[] args) {
+//		System.out.println(Matrix.getMatrix().matrix.length);
+		for(Object o : Matrix.getMatrix().matrix) {
+			
+		}
+	}
+
+}
