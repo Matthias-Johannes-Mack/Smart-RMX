9cb284a88c810fde521bc4202054dae02ea4d62e
Matze <st154919@stud.uni-stuttgart.de>
2020-04-07 20:06:41 +0200

    Fixed build path

---
+++ src/test/java/matrixTest/TestMatrix.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author Matthias Mack 3316380
+ */
+package matrixTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Matthias Mack 3316380
+ */
+public class TestMatrix {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
