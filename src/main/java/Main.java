da1e49ef25bcd34743ee645ca4daa44477a8d803
Matze <st154919@stud.uni-stuttgart.de>
2020-04-10 00:39:05 +0200

    In order to run the programm, the console has to be called

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.IOException;
+
 import connection.SocketConnector;
 import schedular.Schedular;
 import xml.Factory;
@@ -16,18 +19,31 @@ public class Main {
 	 * @param args - Arguments
 	 */
 	public static void main(String[] args) {
-		// open the file
-		XML_IO xml_io = XML_IO.getXML_IO();
-		xml_io.startXmlReadInForUser();
-		// read the xml
-		XML_read xml_read = XML_read.getXML_read();
-		xml_read.processXMLDocument(xml_io.getXML());
+		if (args.length == 0) {
+			try {
+				Process p = Runtime.getRuntime().exec("cmd.exe /c start java -jar "
+						+ (new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()))
+								.getAbsolutePath()
+						+ " cmd");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			// code to be executed
+			// open the file
+			XML_IO xml_io = XML_IO.getXML_IO();
+			xml_io.startXmlReadInForUser();
+			// read the xml
+			XML_read xml_read = XML_read.getXML_read();
+			xml_read.processXMLDocument(xml_io.getXML());
 
-		// create the factory, actionDepot and the matrix
-		Factory.createActionsAndMatrix();
-		// create the connection
-		// schedular MUSS vor Receiver Thread gestartet sein
-		Schedular.getSchedular().startScheduling();
-		SocketConnector.Connect();
+			// create the factory, actionDepot and the matrix
+			Factory.createActionsAndMatrix();
+			// create the connection
+			// schedular MUSS vor Receiver Thread gestartet sein
+			Schedular.getSchedular().startScheduling();
+			SocketConnector.Connect();
+		}
 	}
 }
