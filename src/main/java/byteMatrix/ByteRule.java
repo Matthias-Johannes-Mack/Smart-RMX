afc045533030aa40b69575ca1ab2096fbebb178b
Matze <st154919@stud.uni-stuttgart.de>
2020-04-10 12:37:52 +0200

    Changed ByteRule

--- src/main/java/byteMatrix/ByteRule.java
+++ src/main/java/byteMatrix/ByteRule.java
@@ -1,6 +1,7 @@
 package byteMatrix;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Class for a byte Rule
@@ -34,16 +35,42 @@ public class ByteRule {
 	/**
 	 * Constructor
 	 * 
-	 * @param conditionOne first Condition
-	 * @param conditionTwo second Condition
-	 * @param actions      list containing actions
+	 * @param conditionOneAdress
+	 * @param conditionOneValue
+	 * @param conditionTwoAdress
+	 * @param conditionTwoValue
+	 * @param actions
 	 */
 	public ByteRule(Integer[] conditionOneAdress, Integer[] conditionOneValue, Integer[] conditionTwoAdress,
 			Integer[] conditionTwoValue, ArrayList<Integer[]> actions) {
-		this.conditionOneAdress = conditionOneAdress;
-		this.conditionOneValue = conditionOneValue;
-		this.conditionTwoAdress = conditionTwoAdress;
-		this.conditionTwoValue = conditionTwoValue;
+		// compare indexes, the smaller one is ConditionOneAdress, the bigger one is
+		// condionTwoAdress
+		if (conditionOneAdress[0] < conditionTwoAdress[0]) {
+			this.conditionOneAdress = conditionOneAdress;
+			this.conditionOneValue = conditionOneValue;
+			this.conditionTwoAdress = conditionTwoAdress;
+			this.conditionTwoValue = conditionTwoValue;
+			// compare indexes, the bigger one is ConditionOne
+		} else if (conditionOneAdress[0] > conditionTwoAdress[0]) {
+			this.conditionOneAdress = conditionTwoAdress;
+			this.conditionOneValue = conditionTwoValue;
+			this.conditionTwoAdress = conditionOneAdress;
+			this.conditionTwoValue = conditionOneValue;
+		} else {
+			// 
+			if (conditionOneAdress[1] < conditionTwoAdress[1]) {
+				this.conditionOneAdress = conditionOneAdress;
+				this.conditionOneValue = conditionOneValue;
+				this.conditionTwoAdress = conditionTwoAdress;
+				this.conditionTwoValue = conditionTwoValue;
+			} else if (conditionOneAdress[1] >= conditionTwoAdress[1]) {
+				this.conditionOneAdress = conditionTwoAdress;
+				this.conditionOneValue = conditionTwoValue;
+				this.conditionTwoAdress = conditionOneAdress;
+				this.conditionTwoValue = conditionOneValue;
+			}
+		}
+
 		this.actions = actions;
 	}
 
@@ -83,4 +110,31 @@ public class ByteRule {
 	public Integer[] getConditionTwoValue() {
 		return conditionTwoValue;
 	}
+
+	@Override
+	/**
+	 * Changed the equals method, that it returns true if both conditions are the
+	 * same
+	 */
+	public boolean equals(Object obj) {
+		// if same object
+		if (this == obj) {
+			return true;
+		}
+
+		// if null or not even same class
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+
+		ByteRule objByteRule = (ByteRule) obj;
+		// match the condtionAdress of the first Conditon
+		if (Arrays.equals(getConditionOneAdress(), objByteRule.getConditionOneAdress())) {
+			// match the two Values of Conditions one
+			if (Arrays.equals(getConditionOneValue(), objByteRule.getConditionOneValue())) {
+
+			}
+		}
+	}
+
 }
