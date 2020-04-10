538f5c7dc3c10c1a1dd2dde842792e6c88d4758b
Matze <st154919@stud.uni-stuttgart.de>
2020-04-10 18:53:18 +0200

    Added a console to the application | sysout & syserr are fully printed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 
 import Utilities.Constants;
 import connection.SocketConnector;
+import console.Console;
 import schedular.Schedular;
 import xml.Factory;
 import xml.XML_IO;
@@ -20,28 +21,36 @@ public class Main {
 	 * @param args - Arguments
 	 */
 	public static void main(String[] args) {
-		// check the operating system
-		if (Constants.OPERATING_SYSTEM.contains("Windows")) {
-			if (args.length == 0) {
-				try {
-					Process p = Runtime.getRuntime().exec("cmd.exe /c start java -jar "
-							+ (new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()))
-									.getAbsolutePath()
-							+ " cmd");
-				} catch (IOException e) {
-				}
-			} else {
-				// Execute commands
-				command();
-			}
-		} else if (Constants.OPERATING_SYSTEM.contains("Linux")) {
-		}
+//		// check the operating system
+//		if (Constants.OPERATING_SYSTEM.contains("Windows")) {
+//			if (args.length == 0) {
+//				try {
+//					Process p = Runtime.getRuntime().exec("cmd.exe /c start java -jar "
+//							+ (new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()))
+//									.getAbsolutePath()
+//							+ " cmd");
+//				} catch (IOException e) {
+//				}
+//			} else {
+//				// Execute commands
+//				command();
+//			}
+//		} else if (Constants.OPERATING_SYSTEM.contains("Linux")) {
+//		}
+		command();
 	}
 
 	/**
 	 * Method with commands
 	 */
 	private static void command() {
+		// run the console
+		Console.runConsole();
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+		System.out.println("--------------------------------------------------------------------------------------------");
+		System.out.println("------------------------------------------Smart-RMX-----------------------------------");
+		System.out.println("--------------------------------------------------------------------------------------------");
+		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
 		// open the file
 		XML_IO xml_io = XML_IO.getXML_IO();
 		xml_io.startXmlReadInForUser();
---
+++ src/main/java/console/TestConsole.java
@@ -0,0 +1,12 @@
+package console;
+
+
+
+public class TestConsole {
+	public static void main(String[] args) throws InterruptedException {
+		Console.runConsole();
+		System.out.println("Hey There!");
+	}
+
+
+}
